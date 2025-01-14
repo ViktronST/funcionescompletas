075f8f266925b1b139feaa28469baf8446ae3d17
ViktronST <stoyanovdimitrovviktor@gmail.com>
2025-01-14 20:40:10 +0100

    TablasMultiplicar

--- src/EjecucionTabla.java
+++ src/EjecucionTabla.java
@@ -1,3 +1,14 @@
-public class EjecucionTabla {
+import java.util.Scanner;
 
+public class EjecucionTabla {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce un número para mostrar su tabla de multiplicar: ");
+        int numero = teclado.nextInt();
+        System.out.print("Introduce el tipo de tabla (x, *, por): ");
+        String tipo = teclado.next();
+        HerramientasTabla.mostrarTablaMultiplicar(numero, tipo);
+
+        teclado.close();
+    }
 }
--- src/HerramientasTabla.java
+++ src/HerramientasTabla.java
@@ -1,3 +1,42 @@
 public class HerramientasTabla {
 
+    private static void mostrarTablaMultiplicarX(int numero) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + numero * i);
+        }
+    }
+
+    private static void mostrarTablaMultiplicarAsterisco(int numero) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " * " + i + " = " + numero * i);
+        }
+
+        // for(int i = 1; i <= 10; i++) {
+        //     int resultado = numero * i;
+        //     String operacion = numero + " * " + i;
+        //     System.out.println(operacion + " = " + resultado);
+        // }
+    }
+
+    private static void mostrarTablaMultiplicarPor(int numero) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " por " + i + " = " + numero * i);
+        }
+    }
+
+    public static void mostrarTablaMultiplicar(int numero, String tipo) {
+        switch (tipo) {
+            case "x":
+                mostrarTablaMultiplicarX(numero);
+                break;
+            case "*":
+                mostrarTablaMultiplicarAsterisco(numero);
+                break;
+            case "por":
+                mostrarTablaMultiplicarPor(numero);
+                break;
+            default:
+                System.out.println("Vuelve a intentarlo con un tipo válido (x, *, por)");
+        }
+    }
 }
